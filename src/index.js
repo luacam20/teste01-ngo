a4179d888e448a43dd454fb005b8cffff9d017c0
Luana Campos <luana.cam20@gmail.com>
2021-02-15 14:17:39 -0300

    add new exercises

--- src/index.js
+++ src/index.js
@@ -1,19 +1,47 @@
 var information = [];
 
-const locaStorageCustomers = JSON.parse(localStorage.getItem('customersData'))
+const localStorageCustomers = JSON.parse(localStorage.getItem('customersData'))
 
 if (localStorageCustomers.length >= 1) {
-    information = locaStorageCustomers;
+    information = localStorageCustomers;
+}
+
+function cepSearch() {
+    var cep = document.getElementById("inCep").value.replace(/\D/g, '');
+
+    var url = `https://viacep.com.br/ws/${cep}/json`;
+    var request = new XMLHttpRequest();
+
+    request.open('GET', url);
+
+    request.onerror = function (e) {
+        alert('API OFFLINE OU CEP INVÁLIDO');
+    }
+
+    request.onload = () => {
+        var response = JSON.parse(request.responseText);
+
+        if (response.erro === true) {
+            alert('CEP NÃO ENCONTRADO');
+        } else {
+            var rua = document.getElementById('inStreet');
+            var bairro = document.getElementById('inBairro');
+
+            rua.value = response.logradouro;
+            bairro.value = response.bairro;
+        }
+    }
+
+    request.send();
 }
 
 function save() {
     var name = document.getElementById("inName").value;
     var phone = Number(document.getElementById("inPhone").value);
+    var cep = document.getElementById("inCep").value;
     var street = document.getElementById("inStreet").value;
     var number = Number(document.getElementById("inNumber").value);
-    var cep = Number(document.getElementById("inCep").value);
-    var neighb = document.getElementById("inNboard").value;
-
+    var neighb = document.getElementById("inBairro").value;
 
     if (name == "" || phone == "" || street == "" || number == "" || cep == "" || neighb == "") {
         alert("Infrome corretamente os dados.");
@@ -30,7 +58,7 @@ function save() {
     inStreet.value = "";
     inNumber.value = "";
     inCep.value = "";
-    inNboard.value = "";
+    inBairro.value = "";
 
     inName.focus();
 }
