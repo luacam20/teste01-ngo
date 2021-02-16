19dc2a29adf0dbced7d574645d24826b8f808130
Luana Campos <luana.cam20@gmail.com>
2021-02-16 15:40:59 -0300

    Add new features

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-var information = [];
+var customers = [];
 
 const localStorageCustomers = JSON.parse(localStorage.getItem('customersData'))
 
 if (localStorageCustomers.length >= 1) {
-    information = localStorageCustomers;
+    customers = localStorageCustomers;
 }
 
 function cepSearch() {
@@ -45,13 +45,21 @@ function save() {
 
     if (name == "" || phone == "" || street == "" || number == "" || cep == "" || neighb == "") {
         alert("Infrome corretamente os dados.");
+
+        return;
     }
 
-    information.push({
+    if (customers.find(customer => customer.phone === phone)) {
+        alert("Esse telefone ja foi registrado em nome de outro cliente.");
+
+        return;
+    }
+
+    customers.push({
         name: name, phone: phone, street: street, number: number, cep: cep, neighb: neighb
     });
 
-    localStorage.setItem('customersData', JSON.stringify(information));
+    localStorage.setItem('customersData', JSON.stringify(customers));
 
     inName.value = "";
     inPhone.value = "";
@@ -61,23 +69,28 @@ function save() {
     inBairro.value = "";
 
     inName.focus();
+
+    listar();
 }
 
 function listar() {
     var answer = document.getElementById("inAnswer");
 
-    if (information.length == 0) {
+    if (customers.length == 0) {
         alert("Não há informações salvas.");
         return;
     }
 
     var clist = "";
 
-    for (var i = 0; i < information.length; i++) {
-        clist += "Nome: " + information[i].name + " | " + " Telefone: " + information[i].phone + " | " + " Rua: " + information[i].street + " | " + " n°: " + information[i].number + " | " + " CEP: " + information[i].cep + " | " + " Bairro: " + information[i].neighb + "\n";
+    answer.innerHTML = "";
+
+    for (var i = 0; i < customers.length; i++) {
+        clist = "Nome: " + customers[i].name + " | " + " Telefone: " + customers[i].phone + " | " + " Rua: " + customers[i].street + " | " + " n°: " + customers[i].number + " | " + " CEP: " + customers[i].cep + " | " + " Bairro: " + customers[i].neighb + "\n";
+
+        answer.innerHTML += `<p id="list-item">${clist} <button onclick="remove(${i})" id="delete_button"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg></button></p>`;
     }
 
-    answer.innerHTML = `${clist}`;
 }
 
 function loadLocalStorageData() {
@@ -87,9 +100,33 @@ function loadLocalStorageData() {
 
     var clist = "";
 
+    answer.innerHTML = ``;
+
     for (var i = 0; i < localStorageData.length; i++) {
-        clist += "Nome: " + localStorageData[i].name + " | " + " Telefone: " + localStorageData[i].phone + " | " + " Rua: " + localStorageData[i].street + " | " + " n°: " + localStorageData[i].number + " | " + " CEP: " + localStorageData[i].cep + " | " + " Bairro: " + localStorageData[i].neighb + "\n";
+        clist = "Nome: " + customers[i].name + " | " + " Telefone: " + customers[i].phone + " | " + " Rua: " + customers[i].street + " | " + " n°: " + customers[i].number + " | " + " CEP: " + customers[i].cep + " | " + " Bairro: " + customers[i].neighb + "\n";
+
+        answer.innerHTML += `<p id="list-item">${clist} <button onclick="remove(${i})" id="delete_button"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg></button></p>`;
+    }
+}
+
+function remove(positionOnArray) {
+    var answer = document.getElementById("inAnswer");
+
+    const customerForDelete = customers[positionOnArray];
+    const newCustomersList = customers.filter(customer => customer.phone !== customerForDelete.phone);
+
+    let clist;
+
+    answer.innerHTML = ``;
+
+    for (var j = 0; j < newCustomersList.length; j++) {
+        clist = "Nome: " + newCustomersList[j].name + " | " + " Telefone: " + newCustomersList[j].phone + " | " + " Rua: " + newCustomersList[j].street + " | " + " n°: " + newCustomersList[j].number + " | " + " CEP: " + newCustomersList[j].cep + " | " + " Bairro: " + newCustomersList[j].neighb + "\n";
+
+        answer.innerHTML += `<p id="list-item">${clist} <button onclick="remove(${j})" id="delete_button"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg></button></p>`;
     }
 
-    answer.innerHTML = `${clist}`;
+    customers = newCustomersList;
+
+    localStorage.removeItem('customersData');
+    localStorage.setItem('customersData', JSON.stringify(newCustomersList));
 }
